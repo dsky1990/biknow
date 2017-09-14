d2f93f4aa4d09e8ab3070ff9d9dd9fb0533130d4
dsky1990 <dutian1990@gmail.com>
2017-09-14 23:29:19 +0800

    add eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,20 +20,37 @@ module.exports = {
       'webpack': {
         'config': 'build/webpack.base.conf.js'
       }
-    }
+    },
+    "html/html-extensions": [".vue"]  // consider .html and .we files as HTML
   },
   // add your custom rules here
   'rules': {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
+    "import/no-extraneous-dependencies": ["error", {
+      "devDependencies": true,
+      "optionalDependencies": true
     }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
+    "prefer-arrow-callback": ["error", {
+      "allowNamedFunctions": true
     }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    "comma-dangle": ["error", "never"],
+    "new-cap": ["error", {
+      "properties": false
+    }],
+    "quote-props": ["error", "as-needed", { "keywords": true, "unnecessary": false }],
+    "no-underscore-dangle": ["error", { "allow": ["_this"] }],
+    "no-unused-vars": ["error", { "varsIgnorePattern": "cb" }],
+    "max-len": 0,
+    "no-param-reassign": ["error", { "props": false }],
+    "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+    "linebreak-style": 0,
+    "consistent-return": 0,
+    "guard-for-in": 0,
+    "func-names": 0,
+    "no-console": 0,
+    "one-var": ["error", { "var": "always", "let": "always", "const": "always" }],
+    "prefer-const": ["error", { "destructuring": "all" }],
+    "arrow-body-style": ["error", "as-needed", { "requireReturnForObjectLiteral": true }],
+    "no-eval": ["error", { "allowIndirect": true }],
+    "no-unused-expressions": ["error", { "allowShortCircuit": true, "allowTernary": true }]
   }
 }
